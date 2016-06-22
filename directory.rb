63ae1cc33b9cc485eed82e5e099761271e8c0423
Bart <kluczynskib@gmail.com>
2016-06-22 20:53:44 +0100

    add the manual input

--- directory.rb
+++ directory.rb
@@ -1,18 +1,47 @@
 def input_students
   puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return four times"
   #create an empty array
   students = []
   #get the first name
   name = gets.chomp
+  puts "Please enter your cohort"
+  cohort = gets.chomp
+  puts "Please enter your hobby"
+  hobby = gets.chomp
+  puts "Please enter your country"
+  country = gets.chomp
+
   #while the name is not empty, repeat this code
-  while !name.empty? do
+  while !name.empty? || !cohort.empty? || !hobby.empty? || !country.empty?
   # add the student hash to the array
-  students << {name: name, cohort: :november, hobby: :snarkling, country: :Poland}
+  if cohort.empty? == true
+  cohort = "N/A"
+  end
+
+  if hobby.empty? == true
+  hobby = "N/A"
+  end
+
+  if country.empty? == true
+  country = "N/A"
+  end
+
+  students << {name: name, cohort: cohort, hobby: hobby, country: country}
   puts "Now we have #{students.count} students"
   # get another name from the user
+  puts "Please enter another name of the student or hit return four to exit"
   name = gets.chomp
+  puts "Please enter your cohort"
+  cohort = gets.chomp
+  puts "Please enter your hobby"
+  hobby = gets.chomp
+  puts "Please enter your country"
+  country = gets.chomp
+
+
 end
+
 #return the array of students
 students
 
@@ -29,8 +58,8 @@ index = 0
 
 while students.length > index
 
-  puts "#{index + 1}.#{students[index][:name]}".ljust(20) << " - ".center(10) << "Cohort : #{students[index][:cohort]}".rjust(20) + "||".center(10) + "Hobby : #{students[index][:hobby]}".center(20) +
-  "||".center(10) + "Country : #{students[index][:country]}".center(20)
+  puts "#{index + 1}.#{students[index][:name]}".ljust(10) << " - ".center(10) << "Cohort :".ljust(10) + "#{students[index][:cohort]}".ljust(10) +
+  "||".center(5) + "Hobby :".ljust(10) + "#{students[index][:hobby]}".ljust(10) + "||".center(15) + "Country :".ljust(10) + " #{students[index][:country]}".ljust(10)
 
 index += 1
 end
