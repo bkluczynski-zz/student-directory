57769aaad2cbf5c61cbb35ff48e4bd47d4a2a545
Bart <kluczynskib@gmail.com>
2016-06-23 00:21:53 +0100

    filtered the group by a march cohort

--- directory.rb
+++ directory.rb
@@ -5,8 +5,10 @@ def input_students
   students = []
   #get the first name
   name = gets.chomp
-  puts "Please enter your cohort"
+  puts "Please enter a month of your cohort"
   cohort = gets.chomp
+  #checking for typos
+  #making an array containing all month
   puts "Please enter your hobby"
   hobby = gets.chomp
   puts "Please enter your country"
@@ -28,7 +30,7 @@ def input_students
   country = "N/A"
   end
 
-  students << {name: name, cohort: cohort.to_sym, hobby: hobby, country: country}
+  students << {name: name, cohort: cohort, hobby: hobby, country: country}
   puts "Now we have #{students.count} students"
   # get another name from the user
   puts "Please enter another name of the student or hit return four times to exit"
@@ -62,14 +64,22 @@ def print(students)
 
 index = 0
 
-while students.length > index
+#while students.length > index
 
-  puts "#{index + 1}.#{students[index][:name]}".ljust(10) << " - ".center(10) << "Cohort :".ljust(10) + "#{students[index][:cohort]}".ljust(10) +
-  "||".center(10) + "Hobby :".ljust(10) + "#{students[index][:hobby]}".ljust(10) + "||".center(10) + "Country :".ljust(10) + " #{students[index][:country]}".ljust(10)
+students.map do |group|
 
+
+    if group[:cohort] == "march"
+
+
+  puts "#{index+1}.#{students[index][:name]}".ljust(10) << " - ".center(10) << "Cohort :".ljust(10) +
+  "#{students[index][:cohort]}".ljust(10) + "||".center(10) + "Hobby :".ljust(10) + "#{students[index][:hobby]}".ljust(10) +
+  "||".center(10) + "Country :".ljust(10) + " #{students[index][:country]}".ljust(10)
+
+    end
 index += 1
-end
 
+end
 end
 
 def print_footer(names)
