005ee49aab7bcfc827f60005778385ad8b55f741
Bart <kluczynskib@gmail.com>
2016-06-23 20:33:57 +0100

    add a singular form for students

--- directory.rb
+++ directory.rb
@@ -1,17 +1,11 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the names of the students, a month of your cohort, your hobby and your country of birth. "
   puts "To finish, just hit return four times"
   #create an empty array
   students = []
-  #get the first name
   name = gets.chomp
-  puts "Please enter a month of your cohort"
   cohort = gets.chomp
-  #checking for typos
-  #making an array containing all month
-  puts "Please enter your hobby"
   hobby = gets.chomp
-  puts "Please enter your country"
   country = gets.chomp
 
 
@@ -31,7 +25,8 @@ def input_students
   end
 
   students << {name: name, cohort: cohort, hobby: hobby, country: country}
-  puts "Now we have #{students.count} students"
+
+  print_footer(students)
   # get another name from the user
   puts "Please enter another name of the student or hit return four times to exit"
   name = gets.chomp
@@ -69,21 +64,24 @@ index = 0
 students.map do |group|
 
 
-    if group[:cohort] == "march"
-
-
   puts "#{index+1}.#{students[index][:name]}".ljust(10) << " - ".center(10) << "Cohort :".ljust(10) +
   "#{students[index][:cohort]}".ljust(10) + "||".center(10) + "Hobby :".ljust(10) + "#{students[index][:hobby]}".ljust(10) +
   "||".center(10) + "Country :".ljust(10) + " #{students[index][:country]}".ljust(10)
 
-    end
-index += 1
 
+index += 1
 end
+
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+  if "#{names.count}.".to_i > 1
+    puts "Overall, we have #{names.count} great students"
+  else
+    puts "Overall, we have #{names.count} great student"
+  end
+
 end
 #nothing happens until we call the methods
 students = input_students
