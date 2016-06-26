65fb3c2cba10bc6a620fe6ac3f9c469b84a03c0b
Bart <kluczynskib@gmail.com>
2016-06-26 16:24:13 +0100

    add the block to open and close the file automatically, instead of manual closure

--- directory.rb
+++ directory.rb
@@ -98,23 +98,23 @@ end
 
 def save_students(filnename = "students.csv")
   #open the file for writing
-  file = File.open(@input_saving, "w")
+  file = File.open(@input_saving, "w") do |file|
 #iterate over the array of students
  @students.each do |student|
    student_data = [student[:name],student[:cohort]]
    csv_line = student_data.join(",")
    file.puts csv_line
+  end
  end
- file.close
 end
 
 def load_students(filename = "students.csv")
-  file = File.open(@input_loading, "r")
+  file = File.open(@input_loading, "r") do |file|
   file.readlines.each do |line|
     name, cohort = line.chomp.split(',')
     adding_students(name, cohort)
   end
-  file.close
+end
 end
 
 def adding_students(name, cohort)
